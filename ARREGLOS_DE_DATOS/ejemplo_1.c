1323af648900d44ec438993721a809d3ce2db98a
juanmanuel2005 <151100168+juanmanuel2005@users.noreply.github.com>
2025-04-08 13:54:53 +0000

    se modifico el ejemplo 1

--- ARREGLOS_DE_DATOS/ejemplo_1.c
+++ ARREGLOS_DE_DATOS/ejemplo_1.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    int Datos[5] = {45, 32, 89, 11, 9};
+    int Datos[5] = {45, 32, 89, 11, 9};// se reserva el espacio para 5 datos y se declaran los 5 datos.
     
-    printf("%d\n", Datos[2]);
+    printf("%d\n", Datos[2]);// se imprime el dato en la posision 2 teniendo en cuenta que comienza en 0 siendo 45 el 0, 32 el 1 y asi sucesivamente.
     
     for (int i = 0; i < 5; i++)
 {
-    printf("%d\n - %p\n", Datos[i], &Datos[i]);
+    printf("%d\n - %p\n", Datos[i], &Datos[i]);// imprime los datos y su direccion de memoria.
 }
     return 0;
 }
